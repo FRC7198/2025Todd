7d53e1d85b1a27af377c0474c675ae176551479f
Yosef <ThinkersCap101@gmail.com>
2025-02-03 19:03:54 -0500

    Update elevatorsubsystem.java

--- Todd/src/main/java/frc/robot/subsystems/elevatorsubsystem.java
+++ Todd/src/main/java/frc/robot/subsystems/elevatorsubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
-public class elevatorsubsystem {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class elevatorsubsystem extends SubsystemBase { 
 
 }
