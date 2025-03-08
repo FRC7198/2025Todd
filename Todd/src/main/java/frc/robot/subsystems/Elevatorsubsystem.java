aafde142047977dcb9920f9d0cfa440e2d067dfa
Jesse Boles <jboles@salesforce.com>
2025-03-08 12:12:14 -0500

    merge

--- Todd/src/main/java/frc/robot/subsystems/Elevatorsubsystem.java
+++ Todd/src/main/java/frc/robot/subsystems/Elevatorsubsystem.java
@@ -13,8 +13,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.ElevatorConstants;
 
-
-
 public class Elevatorsubsystem extends SubsystemBase implements AutoCloseable {
 
     Logger logger = Logger.getLogger(Elevatorsubsystem.class.getName());
